cf083ebf850f448cdb34726c322f69eb3e738404
Leeroy Brun <leeroy.brun@gmail.com>
2013-05-23 13:55:40 +0200

    Add error argument to callback & bugfix regarding API sig param

--- example/movie.js
+++ example/movie.js
@@ -1,6 +1,8 @@
 var allocine = require('../lib/allocine-api');
 
-allocine.api('movie', {code: 128188}, function(result) {
+allocine.api('movie', {code: '143067'}, function(error, result) {
+	if(error) { console.log('Error : '+ error); return; }
+
 	console.log('Success !');
 	console.log(result.movie.title);
 });
